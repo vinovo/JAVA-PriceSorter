37428cae54232bf0f3050c6211ca8893d8d9ede9
vino <vino@128.54.176.36>
2018-02-11 22:22:07 -0800

    2018-2-11 22:22:00 updated
    sneaky changes

--- ucsd-cse12-pa5-student/src/cse12pa5student/Sortacle.java
+++ ucsd-cse12-pa5-student/src/cse12pa5student/Sortacle.java
@@ -9,9 +9,19 @@ public class Sortacle {
 	public static List<Item> generateInput(int n) {
 		List<Item> lst = new ArrayList<Item>();
 		Random r = new Random();
-		for (int i = 0; i < n; i++) {
-			String name = "" + (char) (65+Math.random()*26);// + (char) (65+Math.random()*57);
+		lst.add(new Item("ball", 15, 10));
+		lst.add(new Item("ball", 15, 10));
+		lst.add(new Item("ball", 10, 10));
+		lst.add(new Item("fakeball", 15, 10));
+		for (int i = 0; i < n - 4; i++) {
+			String name = "" + (char) (65 + Math.random() * 26);// + (char)
+																// (65+Math.random()*57);
 			lst.add(new Item(name, r.nextInt(20), r.nextInt(3)));
+		}
+		if (lst.size() > n) {
+			for (int i = 0; i < n - lst.size(); i++) {
+				lst.remove(lst.size() - 1);
+			}
 		}
 		return lst;
 	}
@@ -24,7 +34,7 @@ public class Sortacle {
 				return false;
 		}
 		for (int i = 0; i < maybeSorted.size() - 1; i++) {
-			if (maybeSorted.get(i).priceInCents > maybeSorted.get(i+1).priceInCents)
+			if (maybeSorted.get(i).priceInCents > maybeSorted.get(i + 1).priceInCents)
 				return false;
 		}
 		return true;
@@ -33,7 +43,7 @@ public class Sortacle {
 	public static Counterexample isGoodSorter(PriceSorter s) {
 		int numToGenerate = 60;
 		List<Item> original = generateInput(numToGenerate);
-		while (isSortedVersionOf(original, original)){
+		while (isSortedVersionOf(original, original)) {
 			original = generateInput(numToGenerate);
 		}
 		List<Item> input = new ArrayList<Item>(original);
@@ -55,5 +65,5 @@ public class Sortacle {
 		}
 		return count;
 	}
-	
+
 }
